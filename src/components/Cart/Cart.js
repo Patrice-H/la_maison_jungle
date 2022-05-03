585a5251099b813ef048b3dab47021a35c86d0e8
Patrice-H <https://github.com/>
2022-05-03 18:51:31 +0200

    Update cart component and add styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,20 @@
 import React from "react";
+import './Cart.css';
 
 const Cart = () => {
     const monsteraPrice = 8;
     const ivyPrice = 10;
     const flowersPrice = 15;
     return (
-      <div>
-        <ul>
-          <li>Monstera : { monsteraPrice }€</li>
-          <li>Lierre : { ivyPrice }€</li>
-          <li>Bouquet de fleurs : { flowersPrice}€</li>
-        </ul>
-        <p>Total : { monsteraPrice + ivyPrice + flowersPrice }€</p>
-      </div>
+        <div className='cart'>
+            <h2>Panier</h2>
+            <ul>
+                <li>Monstera : { monsteraPrice }€</li>
+                <li>Lierre : { ivyPrice }€</li>
+                <li>Bouquet de fleurs : { flowersPrice}€</li>
+            </ul>
+            <p>Total : { monsteraPrice + ivyPrice + flowersPrice }€</p>
+        </div>
     );
   }
 
