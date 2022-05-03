8b6098deaf8e45a0a8d44bf6083e31761b868f70
Patrice-H <https://github.com/>
2022-05-03 18:22:44 +0200

    Update banner component and add styles

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,14 @@
 import React from "react";
+import logo from '../../assets/logo.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-    <h1>La maison jungle</h1>
+        <header className='banner'>
+            <img src={ logo } alt='La maison jungle' className='banner-logo' />
+            <h1 className='banner-title'>La maison jungle</h1>
+        </header>
     );
 }
 
-export default Banner
+export default Banner;
