0503d5ebf5f4f6794b93142f94915c84e38e25bc
Patrice-H <https://github.com/>
2022-05-04 17:34:58 +0200

    Update App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Banner from '../Banner/Banner';
 import Cart from "../Cart/Cart";
+import ShoppingList from "../ShoppingList/ShoppingList";
 import './App.css';
 
 const App = () => {
   return (
-    <><Banner /><Cart /></>
+    <>
+      <Banner />
+      <div className='layout'>
+        <Cart />
+        <ShoppingList />
+      </div>
+      
+    </>
   );
 }
 
