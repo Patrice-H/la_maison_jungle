60e98dae9a6c9d08643f2917d942fd7f792a195d
Patrice-H <https://github.com/>
2022-05-04 17:21:54 +0200

    Add data

---
+++ src/datas/plantList.js
@@ -0,0 +1,101 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		price: 15,
+		water: 3,
+		light: 2,
+		cover: 'monstera.jpg',
+		isBestSale: true,
+		isSpecialOffer: false
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		price: 16,
+		water: 1,
+		light: 3,
+		cover: 'ficus-lyrata.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		price: 9,
+		water: 2,
+		light: 1,
+		cover: 'pothos-argente.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '4kk',
+		price: 4,
+		water: 2,
+		light: 2,
+		cover: 'menthe.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		price: 25,
+		water: 1,
+		light: 3,
+		cover: 'olivier.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'calathea',
+		category: 'classique',
+		id: '6uo',
+		price: 20,
+		water: 3,
+		light: 2,
+		cover: 'calathea.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		price: 5,
+		water: 3,
+		light: 2,
+		cover: 'basilique.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		price: 6,
+		water: 1,
+		light: 2,
+		cover: 'cactus.jpg',
+		isBestSale: false,
+		isSpecialOffer: false
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		price: 8,
+		water: 1,
+		light: 2,
+		cover: 'succulente.jpg',
+		isBestSale: false,
+		isSpecialOffer: true
+	}
+];
