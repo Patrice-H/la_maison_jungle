0a083f6510b19fbc142b08dfdbbd68580630395c
Patrice-H <https://github.com/>
2022-05-09 13:47:21 +0200

    Add buttons to open or close cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './Cart.css';
 
 const Cart = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const monsteraPrice = 8;
     const ivyPrice = 10;
     const flowersPrice = 15;
     return (
+        isOpen ? 
         <div className='cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
             <ul>
                 <li>Monstera : { monsteraPrice }€</li>
@@ -14,7 +17,8 @@ const Cart = () => {
                 <li>Bouquet de fleurs : { flowersPrice}€</li>
             </ul>
             <p>Total : { monsteraPrice + ivyPrice + flowersPrice }€</p>
-        </div>
+        </div> : 
+        <button onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
     );
   }
 
