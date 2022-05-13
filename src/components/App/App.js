a09e37c630c38d0e58879170007dae7137c924f8
Patrice-H <https://github.com/Patrice-H>
2022-05-13 17:42:13 +0200

    Add a functionality to save the cart in local storage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Banner from '../Banner/Banner';
 import Cart from "../Cart/Cart";
 import Footer from "../Footer/Footer";
@@ -7,8 +7,11 @@ import ShoppingList from "../ShoppingList/ShoppingList";
 import './App.css';
 
 const App = () => {
-
-  const [cart, updateCart] = useState([]);
+  const savedCart = localStorage.getItem('cart');
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
+  useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart));
+	}, [cart]);
 
   return (
     <>
